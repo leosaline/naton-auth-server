2b0b6257223509c9784c8449d42c806ee0dd8a1f
leosaline <leosaline@gmail.com>
2020-01-12 15:54:54 -0300

    test eureka info

--- src/main/java/com/saline/naton/NatonAuthServerApplication.java
+++ src/main/java/com/saline/naton/NatonAuthServerApplication.java
@@ -3,10 +3,18 @@ package com.saline.naton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class NatonAuthServerApplication {
+	
+	@RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(NatonAuthServerApplication.class, args);
